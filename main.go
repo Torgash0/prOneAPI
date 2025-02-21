2f0b0a409b110e37a8da18eef1fc9d43db40933d
japa <gaydaev1654@icloud.com>
2025-02-21 04:52:10 +0300

    pr2

--- main.go
+++ main.go
@@ -2,43 +2,44 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"net/http"
-	"sync"
-
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
-var (
-	task   string
-	taskMu sync.Mutex
-)
-
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	taskMu.Lock()
-	defer taskMu.Unlock()
-	fmt.Fprintf(w, "Hello, %s!", task)
-}
-
-func SetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	var req struct {
-		Task string `json:"task"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	taskMu.Lock()
-	defer taskMu.Unlock()
-	task = req.Task
-	w.WriteHeader(http.StatusOK)
+	if err := DB.Create(&newTask).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []Task
+	if err := DB.Find(&tasks).Error; err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func main() {
+
+	InitDB()
+	DB.AutoMigrate(&Task{})
+
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	router.HandleFunc("/api/set-task", SetTaskHandler).Methods("POST")
+	router.HandleFunc("/api/tasks", CreateTaskHandler).Methods("POST")
+	router.HandleFunc("/api/tasks", GetTasksHandler).Methods("GET")
 	http.ListenAndServe(":8080", router)
 }
